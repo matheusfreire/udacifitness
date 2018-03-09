5dfc7ce55d39de295063ab1ee6416209d4baa4b7
matheusfreire <matheus.freire@trixti.com.br>
2018-03-09 17:02:41 -0300

    Update for show components

---
+++ components/AddEntry.js
@@ -0,0 +1,57 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+import Slider from './Slider'
+import Stepper from './Stepper'
+
+import {getMetricMetaInfo} from '../utils/helpers'
+
+export default class AddEntry extends Component {
+
+    state = {
+        run: 0,
+        bike: 0,
+        swim: 0,
+        sleep: 0,
+        eat: 0
+    }
+
+    increment = (metric) => {
+        const {max, step} = getMetricMetaInfo(metric)
+
+        this.setState((state) => {
+            const count = state[metric] + step
+            return {...state, [metric]: count > max ? max: count}
+        })
+    }
+
+    decrement = (metric) => {
+        this.setState((state) => {
+            const count = state[metric] - getMetricMetaInfo(metric).step
+            return {...state, [metric]: count < 0 ? 0: count}
+        })
+    }
+
+    slide = (metric, value) => {
+        this.setState(() => ({
+            [metric]: value
+        }))
+    }
+
+    render(){
+        const metaInfo = getMetricMetaInfo()
+        return (
+            <View>
+                {Object.keys(metaInfo).map((key) => {
+                    const {getIcon, type, ...rest} = metaInfo[key]
+                    const key = this.state[key]
+                    return (
+                        <View key={key}>
+                            {getIcon()}
+                        </View>
+                    )
+                })}
+            </View>
+        )
+    }
+}
