aa4821e358d718b665be92ed3184a7a78fd39260
matheusfreire <trixti@matheus.trixti.com.br>
2018-03-14 16:34:47 -0300

    Add stacknavigator

--- components/EntryDetail.js
+++ components/EntryDetail.js
@@ -1,9 +1,20 @@
-import React, {Component} from 'react'
-import {View, Text} from 'react-native'
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
 
 class EntryDetail extends Component {
+
+    static navigationOptions = ({ navigation }) => {
+        const { entryId } = navigation.state.params
+        const year = entryId.slice(0, 4)
+        const month = entryId.slice(5, 7)
+        const day = entryId.slice(8)
+
+        return {
+            title: `${day}/${month}/${year}`
+        }
+    }
     render() {
-        return(
+        return (
             <View>
                 <Text>Entry Detail - {this.props.navigation.state.params.entryId}</Text>
             </View>
